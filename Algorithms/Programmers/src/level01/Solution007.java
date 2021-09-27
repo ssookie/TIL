39da334a546867a39028b54b0a9bb2ddd336479d
ssookie <hyunsue77@gmail.com>
2021-09-27 22:40:58 +0900

    Stack 풀이방법 추가

--- Algorithms/Programmers/src/level01/Solution007.java
+++ Algorithms/Programmers/src/level01/Solution007.java
@@ -1,6 +1,7 @@
 package level01;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * [2019 카카오 개발자 겨울 인턴십] 크레인 인형뽑기 게임
@@ -8,6 +9,9 @@ import java.util.ArrayList;
  * Array/ArrayList/Stack
  */
 public class Solution007 {
+    /**
+     * ArrayList 이용
+     */
     static int solution(int[][] board, int[] moves) {
         int answer = 0;
         ArrayList<Integer> basket = new ArrayList<Integer>();
@@ -39,8 +43,36 @@ public class Solution007 {
         return result;
     }
 
+    /**
+     * Stack 이용
+     */
+    static int solutionStack(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> basket = new Stack<>(); // declare int type stack
+
+        for (int i=0; i<moves.length; i++) {
+            for (int j=0; j< board.length; j++) {
+                if (board[j][moves[i]-1] != 0) {
+                    // check basket
+                    if (!basket.isEmpty() && basket.peek() == board[j][moves[i]-1]) {
+                        basket.pop();
+                        answer = answer + 2;
+                    } else {
+                        basket.push(board[j][moves[i]-1]);
+                    }
+                    // remove from board
+                    board[j][moves[i]-1] = 0;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-        int answer = solution(new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}, new int[]{1,5,3,5,1,2,1,4});
+        // int answer = solution(new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}, new int[]{1,5,3,5,1,2,1,4});
+        int answer = solutionStack(new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}, new int[]{1,5,3,5,1,2,1,4});
         System.out.println(answer);
     }
 }
