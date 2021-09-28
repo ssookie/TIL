1061bfebbed39e5a5edd189d71e056a463eadb49
ssookie <hyunsue77@gmail.com>
2021-09-28 22:41:31 +0900

    big-o 고려하여 for 문으로 바꾸기

--- Algorithms/Programmers/src/level01/Solution008.java
+++ Algorithms/Programmers/src/level01/Solution008.java
@@ -7,9 +7,6 @@ import java.util.Arrays;
  * https://programmers.co.kr/learn/courses/30/lessons/77484
  */
 public class Solution008 {
-
-    public static int i = 0;
-
     static int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = {0, 0};
         int[] lottoRanking = {6, 6, 5, 4, 3, 2, 1, 0};
@@ -17,12 +14,11 @@ public class Solution008 {
         // 로또 당첨 번호 갯수 확인
         int match = 0;
         int unknown = 0;
-        for (i=0; i<6; i++) {
-//            if (Arrays.asList(lottos).contains(win_nums[i])) {
-//                match = match + 1;
-//            }
-            if (Arrays.stream(lottos).anyMatch(x -> x == win_nums[i])) {
-                match ++;
+        for (int i=0; i<6; i++) {
+            for (int j=0; j<6; j++) {
+                if (lottos[i] == win_nums[j]) {
+                    match ++;
+                }
             }
             if (lottos[i] == 0) {
                 unknown ++;
