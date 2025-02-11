32f95dc63d2c46c5158efae4fb8f39f77db3f34a
ssookie <hyunsue77@gmail.com>
2025-02-11 16:16:54 +0900

    가상 면접 사례로 배우는 대규모 시스템 설계 Start!

---
+++ SystemArchitecture/system-design-interview/src/main/java/com/ssookie/design/SystemDesignApplication.java
@@ -0,0 +1,13 @@
+package com.ssookie.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDesignApplication.class, args);
+	}
+
+}
---
+++ SystemArchitecture/system-design-interview/src/test/java/com/ssookie/design/SystemDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssookie.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
